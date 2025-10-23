a3826926c14eb8b7411375b56ded7175c7390c6a
franwanderley <franwanderley@outlook.com>
2025-10-23 10:13:34 -0300

    ajust charge-controller

--- src/charge/charge.controller.ts
+++ src/charge/charge.controller.ts
@@ -6,38 +6,49 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ChargeService } from './charge.service';
 import { CreateChargeDto } from './dto/create-charge.dto';
 import { UpdateChargeDto } from './dto/update-charge.dto';
 import { UUID } from 'crypto';
 
-@Controller('charge')
+@Controller('charges')
 export class ChargeController {
   constructor(private readonly chargeService: ChargeService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createChargeDto: CreateChargeDto) {
     return this.chargeService.create(createChargeDto);
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.chargeService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: UUID) {
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id', new ParseUUIDPipe()) id: UUID) {
     return this.chargeService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: UUID, @Body() updateChargeDto: UpdateChargeDto) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  update(
+    @Param('id', new ParseUUIDPipe()) id: UUID,
+    @Body() updateChargeDto: UpdateChargeDto,
+  ) {
     return this.chargeService.update(id, updateChargeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: UUID) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id', new ParseUUIDPipe()) id: UUID) {
     return this.chargeService.remove(id);
   }
 }
